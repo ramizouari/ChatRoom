8acbc69bca09b9e0ab0637204badbb6a6adc3f24
ramizouari <zouari.rami@yahoo.com>
2020-04-23 01:46:29 +0100

    Added User class for storing each user/socket's informations (currently only username)

---
+++ server/User.java
@@ -0,0 +1,36 @@
+package server;
+import java.net.*;
+
+
+//This class contains the information of a user: his username and the socket
+//A one to one correspendence between each socket and each username is assumed
+public class User {
+    Socket sock;
+    String username;
+    public User(String name,Socket s)
+    {
+        sock=s;
+        username=name;
+    }
+    public String getUserName()
+    {
+        return username;
+    }
+    public Socket getSocket()
+    {
+        return sock;
+    }
+
+    //this method will assume the one to one correspendence
+    public boolean equals(Object o)
+    {   
+        if(o instanceof User)
+            return ((User)o).username.equals(username);
+        else if(o instanceof String)
+            return o.equals(username);
+        else if(o instanceof Socket)
+            return o.equals(sock);
+        return false;
+
+    }
+}
