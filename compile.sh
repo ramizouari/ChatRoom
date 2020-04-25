67090ed873f54290b31468d9764c39445e06565b
ramizouari <zouari.rami@yahoo.com>
2020-04-25 03:49:18 +0100

    Description to the script

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#this script will compile Client and Server files place each to its target location 
+#this script will compile Client and Server files then place each to its target location 
 #specified by the variables below:
-#if you add files to the project that are dependant to ChatRoomServer.java,ChatRoom.java or ChatRoomGUI.java
+#if you add files to the project that are not dependant to ChatRoomServer.java,ChatRoom.java or ChatRoomGUI.java
 #you must add them manually to the script or compile them then  move them manually to the target directory
 target_server_location="../ChatRoomBin/Server"
 target_client_location="../ChatRoomBin/Client"
